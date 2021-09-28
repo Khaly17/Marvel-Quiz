7c6d4e56ce346d336768253c6f41836c3306777e
Khaly17 <diengkhaly17@gmail.com>
2021-09-28 13:35:16 +0000

    Les évènements onMouseOver et onMouseOut

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -16,12 +16,30 @@ const Landing = () => {
         }, 1000)
     },[])
 
+    const setLeftImg = () => {
+        refwolverine.current.classList.add("leftImg")
+
+    }
+
+    const setRigthImg = () => {
+        refwolverine.current.classList.add("rightImg")
+    }
+
+    const clearImg = () => {
+        if(refwolverine.current.classList.contains("leftImg")){
+            refwolverine.current.classList.remove("leftImg")
+        }
+        else  if(refwolverine.current.classList.contains("rightImg")){
+            refwolverine.current.classList.remove("rightImg")
+        }
+    }
+
     const displayBtn = btn && (
         <>
-            <div className="leftBox">
+            <div onMouseOver={setLeftImg} onMouseOut={clearImg} className="leftBox">
                 <button className="btn-welcome">Inscription</button>
             </div>
-            <div className="rightBox">
+            <div onMouseOver={setRigthImg} onMouseOut={clearImg} className="rightBox">
                 <button className="btn-welcome">Connexion</button>
             </div>
         </>
