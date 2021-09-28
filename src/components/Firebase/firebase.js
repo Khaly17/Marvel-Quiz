7e60c1378df2298f46514c88ec4ae6e15d785ac7
Khaly17 <diengkhaly17@gmail.com>
2021-09-28 15:33:07 +0000

    Configuration Firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyCPUFWwO-tH_5S5iJ4YfvEqj9aenH_QNoU",
+    authDomain: "marvel-quiz-2ec2a.firebaseapp.com",
+    projectId: "marvel-quiz-2ec2a",
+    storageBucket: "marvel-quiz-2ec2a.appspot.com",
+    messagingSenderId: "1024982783058",
+    appId: "1:1024982783058:web:a041d9a8ea38600eb2fb8b"
+};
+
+class Firebase{
+    constructor() {
+        app.initializeApp(config)
+    }
+}
+
+export default Firebase
