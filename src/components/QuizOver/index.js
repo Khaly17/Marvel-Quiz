99abef6ae66d8ce8e9cb62cc1811ad7106bad862
Khaly17 <diengkhaly17@gmail.com>
2021-10-08 15:49:18 +0000

    Affichage de la data dans le Modal

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -83,6 +83,10 @@ const QuizOver = React.forwardRef(
             setLoading(true)
         }
 
+        const capitalizeFirstLetter = string =>{
+            return string.charAt(0).toUpperCase() + string.slice(1)
+        }
+
         const averageGrade = maxQuestions / 2
 
         if(score < averageGrade){
@@ -185,10 +189,39 @@ const QuizOver = React.forwardRef(
                         <h2>{characterInfos.data.results[0].name}</h2>
                     </div>
                     <div className="modalBody">
-                        <h3>Titre 2</h3>
+                        <div className="comicImage">
+                            <img
+                                src={characterInfos.data.results[0].thumbnail.path+'.'+characterInfos.data.results[0].thumbnail.extension}
+                                alt={characterInfos.data.results[0].name}
+                            />
+                            {characterInfos.attributionText}
+                        </div>
+                        <div className="comicDetails">
+                            <h3>Description</h3>
+                            {
+                                characterInfos.data.results[0].description ?
+                                    <p>{characterInfos.data.results[0].description}</p>
+                                    :
+                                   <p>Description indisponible ...</p>
+                            }
+                            <h3>Plus d'Infos</h3>
+                            {
+                                characterInfos.data.results[0].urls &&
+                                characterInfos.data.results[0].urls.map((url,index)=>{
+                                    return <a
+                                        key={index}
+                                        href={url.url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        {capitalizeFirstLetter(url.type)}
+                                    </a>
+                                })
+                            }
+                        </div>
                     </div>
                     <div className="modalFooter">
-                        <button className="modalBtn">Fermer</button>
+                        <button className="modalBtn" onClick={hideModal}>Fermer</button>
                     </div>
                 </>
             )
